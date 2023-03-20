3f0056125ab8b5d7a14a8dccb1b0c7a17d417b62
Андрей Матей <mateiandav@gmail.com>
2023-03-20 11:23:24 +0400

    minor fix

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -48,8 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'debug_toolbar',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -59,8 +58,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'yatube.urls'
