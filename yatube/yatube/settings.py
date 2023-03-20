f66f65d67f581c94d4b46638bfdabdd2a8c1707a
Андрей Матей <mateiandav@gmail.com>
2023-03-20 20:10:25 +0400

    STATIC_ROOT added

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,6 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
